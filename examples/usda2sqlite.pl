75321c0980792ab44c7f64c901c89a8d91e9f550
Ioan Rogers <ioanr@cpan.org>
2013-06-20 17:47:24 -0700

    Added tidyall.ini, updated perl*rc files, tidied

--- examples/usda2sqlite.pl
+++ examples/usda2sqlite.pl
@@ -15,14 +15,14 @@ my $db_file;
 my $usda = Parse::USDA::NNDB->new;
 
 sub create_tables {
-    my $sql_file = catfile( $dir, 'usda_sqlite.sql' );
+    my $sql_file = catfile($dir, 'usda_sqlite.sql');
 
     say 'Creating database';
     open my $fh, '<', $sql_file
       or die "Failed to open SQL file: $!";
 
     system "sqlite3 $db_file < $sql_file";
-    if ( $? != 0 ) {
+    if ($? != 0) {
         die "system failed: $?";
     }
 
@@ -30,9 +30,9 @@ sub create_tables {
 }
 
 sub get_keys {
-    my ( $table ) = @_;
-    my $cols = $usda->get_columns_for( $table );
-    if ( !defined $cols ) {
+    my ($table) = @_;
+    my $cols = $usda->get_columns_for($table);
+    if (!defined $cols) {
         die;
     }
     return $cols;
@@ -40,14 +40,14 @@ sub get_keys {
 
 sub normalise_keys {
     my $item = shift;
-    my %data = map { lc( $_ ) => $item->{$_} } keys %{$item};
+    my %data = map { lc($_) => $item->{$_} } keys %{$item};
     return \%data;
 }
 
 # from orlite
 sub normalise_table_name {
     my $table = shift;
-    if ( $table ne lc $table ) {
+    if ($table ne lc $table) {
         $table =~ s/([a-z])([A-Z])/${1}_${2}/g;
         $table =~ s/_+/_/g;
     }
@@ -62,15 +62,15 @@ sub do_table {
 
     say "### $table ###";
 
-    $usda->table( $table );
+    $usda->table($table);
 
-    my $pkg = normalise_table_name( $table );
+    my $pkg = normalise_table_name($table);
 
     NutDB->begin;
-    while ( my $rd = $usda->get_line ) {
-        my $data = normalise_keys( $rd );
-        if ( $pkg->can( 'create' ) ) {
-            my $n = $pkg->create( %{$data} );
+    while (my $rd = $usda->get_line) {
+        my $data = normalise_keys($rd);
+        if ($pkg->can('create')) {
+            my $n = $pkg->create(%{$data});
         } else {
             die "$pkg has no create method - DID YOU MAKE ANOTHER TYPO!?";
         }
@@ -80,17 +80,17 @@ sub do_table {
     print "\n\n";
 }
 
-Log::Any::Adapter->set( 'ScreenColoredLevel', min_level => 'debug', );
+Log::Any::Adapter->set('ScreenColoredLevel', min_level => 'debug',);
 
-$dir = dirname( $0 );
-$db_file = catfile( $dir, 'usda_nut.sqlite' );
+$dir = dirname($0);
+$db_file = catfile($dir, 'usda_nut.sqlite');
 
-my $db = ORLite->import( {
-        package => 'NutDB',
-        file    => $db_file,
-        create  => \&create_tables,
-} );
+my $db = ORLite->import({
+    package => 'NutDB',
+    file    => $db_file,
+    create  => \&create_tables,
+});
 
-foreach my $table ( @{$usda->tables} ) {
+foreach my $table (@{$usda->tables}) {
     do_table($table);
 }
--- lib/Parse/USDA/NNDB.pm
+++ lib/Parse/USDA/NNDB.pm
@@ -10,6 +10,7 @@ use Archive::Zip qw/ :ERROR_CODES :CONSTANTS /;
 use Carp qw/croak/;
 use File::Fetch;
 use File::HomeDir;
+
 #use IO::Uncompress::Unzip;
 use Log::Any;
 use methods;
@@ -32,6 +33,7 @@ This is ithe directory into which the data files will be downloaded.
 Defaults to $HOME/.cache/usda_nndb
 
 =cut
+
 has base_dir => ( is => 'lazy' );
 has _data_dir => ( is => 'lazy' );
 has _src_uri  => ( is => 'lazy' );
@@ -44,14 +46,18 @@ has _csv => (is => 'lazy', predicate => 1, clearer => 1);
 An arrayref of all the known tables.
 
 =cut
+
 has tables => (
-    is => 'ro',
-    default => sub {[
-        qw/DATSRCLN FD_GROUP LANGUAL LANGDESC FOOTNOTE NUTR_DEF SRC_CD DATA_SRC DERIV_CD FOOD_DES NUT_DATA WEIGHT/
-    ]},
+    is      => 'ro',
+    default => sub {
+        [
+            qw/DATSRCLN FD_GROUP LANGUAL LANGDESC FOOTNOTE NUTR_DEF SRC_CD DATA_SRC DERIV_CD FOOD_DES NUT_DATA WEIGHT/
+        ];
+    },
 );
 
-method _build_base_dir {
+method _build_base_dir () {
+
     # TODO better cross-platform defaults
     my $dir = path(File::HomeDir->my_home, '.cache/usda_nndb');
     if (!$dir->exists) {
@@ -60,7 +66,7 @@ method _build_base_dir {
     return $dir;
 }
 
-method _build__data_dir {
+method _build__data_dir () {
     my $dir = $self->base_dir->child('sr24');
     if (!$dir->exists) {
         $dir->mkpath;
@@ -68,39 +74,41 @@ method _build__data_dir {
     return $dir;
 }
 
-method _build__src_uri {
-    URI->new( 'http://www.ars.usda.gov/SP2UserFiles/Place/12354500/Data/SR24/dnload/sr24.ZIP' ),
+method _build__src_uri () {
+    URI->new(
+        'http://www.ars.usda.gov/SP2UserFiles/Place/12354500/Data/SR24/dnload/sr24.ZIP'
+    );
 }
 
-method _build__src_file {
+method _build__src_file () {
     $self->base_dir->child('sr24.ZIP');
 }
 
-method _build__csv {
-    return Text::CSV_XS->new( {
-            quote_char          => '~',
-            escape_char         => '~',
-            sep_char            => '^',
-            empty_is_undef      => 1,
-            binary              => 1,
-    } );
+method _build__csv () {
+    return Text::CSV_XS->new({
+        quote_char     => '~',
+        escape_char    => '~',
+        sep_char       => '^',
+        empty_is_undef => 1,
+        binary         => 1,
+    });
 }
 
-method _log_croak($msg) {
+method _log_croak ($msg) {
     $self->log->crit($msg);
     croak $msg;
 }
 
-method _log_croakf(@args) {
+method _log_croakf (@args) {
     my $msg = sprintf shift @args, @args;
     $self->_log_croak($msg);
 }
 
-method _get_file_path_for($table) {
+method _get_file_path_for ($table) {
     my $file = $self->_data_dir->child("$table.txt");
-    $self->logger->debug( "Using path [$file] for '$table'" );
-    if ( !$file->exists ) {
-          $self->_extract_data;
+    $self->logger->debug("Using path [$file] for '$table'");
+    if (!$file->exists) {
+        $self->_extract_data;
     }
 
     return $file;
@@ -112,56 +120,60 @@ Returns an arrayref of the column names used in this table, or undef if the
 table is unknown
 
 =cut
-method get_columns_for($table) {
 
-    given ( $table ) {
-        when ( /^FOOD_DES$/i ) {
+method get_columns_for ($table) {
+
+    given ($table) {
+        when (/^FOOD_DES$/i) {
             return [
                 qw/NDB_No FdGrp_Cd Long_Desc Shrt_Desc ComName ManufacName Survey Ref_desc Refuse SciName N_Factor Pro_Factor Fat_Factor CHO_Factor/
             ];
         }
-        when ( /^FD_GROUP$/i ) {
+        when (/^FD_GROUP$/i) {
             return [qw/FdGrp_Cd FdGrp_Desc/];
         }
-        when ( /^LANGUAL$/i ) {
+        when (/^LANGUAL$/i) {
             return [qw/NDB_No Factor_Code/];
         }
-        when ( /^LANGDESC$/i ) {
+        when (/^LANGDESC$/i) {
             return [qw/Factor_Code Description/];
         }
-        when ( /^NUT_DATA$/i ) {
+        when (/^NUT_DATA$/i) {
             return [
                 qw/NDB_No Nutr_No Nutr_Val Num_Data_Pts Std_Error Src_Cd Deriv_Cd Ref_NDB_No Add_Nutr_Mark Num_Studies Min Max DF Low_EB Up_EB Stat_cmt CC/
             ];
 
         }
-        when ( /^NUTR_DEF$/i ) {
+        when (/^NUTR_DEF$/i) {
             return [qw(Nutr_No Units Tagname NutrDesc Num_Dec SR_Order)];
         }
-        when ( /^SRC_CD$/i ) {
+        when (/^SRC_CD$/i) {
             return [qw(Src_Cd SrcCd_Desc)];
         }
-        when ( /^DERIV_CD$/i ) {
+        when (/^DERIV_CD$/i) {
             return [qw(Deriv_Cd Deriv_Desc)];
         }
-        when ( /^WEIGHT$/i ) {
-            return [qw(NDB_No Seq Amount Msre_Desc Gm_Wgt Num_Data_Pts Std_Dev)];
+        when (/^WEIGHT$/i) {
+            return [
+                qw(NDB_No Seq Amount Msre_Desc Gm_Wgt Num_Data_Pts Std_Dev)];
         }
-        when ( /^FOOTNOTE$/i ) {
+        when (/^FOOTNOTE$/i) {
             return [qw(NDB_No Footnt_No Footnt_Typ Nutr_No Footnt_Txt)];
         }
-        when ( /^DATSRCLN$/i ) {
+        when (/^DATSRCLN$/i) {
             return [qw(NDB_No Nutr_No DataSrc_ID)];
         }
-        when ( /^DATA_SRC$/i ) {
-            return [qw(DataSrc_ID Authors Title Year Journal Vol_City Issue_State Start_Page End_Page)];
+        when (/^DATA_SRC$/i) {
+            return [
+                qw(DataSrc_ID Authors Title Year Journal Vol_City Issue_State Start_Page End_Page)
+            ];
         }
 
-#when ( /^ABBREV$/i ) {
-#    return [
-#        qw(NDB_No Shrt_Desc Water Energ_Kcal Protein Lipit_Tot Ash Carbonhydrt Fiber_TD Sugar_Tot Calcium Iron Magnesium Phosphorus Potassium Sodium Zinc Copper Manganese Selenium Vit_C Thiamin Riboflavin Niacin Panto_acid Vit_B6 Folate_Tot Folic_acid Food_Folate Folate_DFE Choline_total Vit_B12 Vit_A_IU Vit_A_RAE Retinol Alpha_Carot Beta_Carot Beta_Crypt Lycopene Lut_and_Zea Vit_E Vit_K FA_Sat FA_Mono FA_Poly Cholestrl GmWt_1 GmWt_Desc1 GmWt_2 GmWt_Desc2 Refuse_Pct)
-#      ];
-#}
+        #when ( /^ABBREV$/i ) {
+        #    return [
+        #        qw(NDB_No Shrt_Desc Water Energ_Kcal Protein Lipit_Tot Ash Carbonhydrt Fiber_TD Sugar_Tot Calcium Iron Magnesium Phosphorus Potassium Sodium Zinc Copper Manganese Selenium Vit_C Thiamin Riboflavin Niacin Panto_acid Vit_B6 Folate_Tot Folic_acid Food_Folate Folate_DFE Choline_total Vit_B12 Vit_A_IU Vit_A_RAE Retinol Alpha_Carot Beta_Carot Beta_Crypt Lycopene Lut_and_Zea Vit_E Vit_K FA_Sat FA_Mono FA_Poly Cholestrl GmWt_1 GmWt_Desc1 GmWt_2 GmWt_Desc2 Refuse_Pct)
+        #      ];
+        #}
         default {
             return;
         }
@@ -176,17 +188,18 @@ and opens the file ready for parsing. You must call this before B<get_line>.
 Returns true on success, throws an exception on error.
 
 =cut
-method table($table) {
 
-    my $file_path = $self->_get_file_path_for( $table );
+method table ($table) {
+
+    my $file_path = $self->_get_file_path_for($table);
 
     open my $fh, '<:encoding(iso-8859-1)', $file_path
-        or $self->_log_croak( "Could not open [$file_path]: $!" );
+      or $self->_log_croak("Could not open [$file_path]: $!");
 
-    my $column_names = $self->get_columns_for( $table )
+    my $column_names = $self->get_columns_for($table)
       or $self->_log_croak("Couldn't find columns for [$table]");
 
-    $self->_csv->column_names( $column_names );
+    $self->_csv->column_names($column_names);
     $self->_fh($fh);
 
     return 1;
@@ -200,49 +213,56 @@ Returns the next line in the data file and returns a hashref
 Returns undef when the file is finished or if something goes wrong.
 
 =cut
-method get_line {
+
+method get_line () {
     if (!$self->_has_fh) {
-        $self->_log_croak('No active filehandle. Did you call \'table\' first?');
+        $self->_log_croak(
+            'No active filehandle. Did you call \'table\' first?');
+
         # TODO should check the handle is the *right* handle
     }
     if (!$self->_has_csv) {
         $self->_log_croak('No csv object. Did you call \'table\' first?');
     }
 
-    my $row = $self->_csv->getline_hr( $self->_fh );
+    my $row = $self->_csv->getline_hr($self->_fh);
 
-    if ( $self->_csv->eof ) {
-        $self->logger->debug( 'Closing file' );
+    if ($self->_csv->eof) {
+        $self->logger->debug('Closing file');
         $self->_fh->close;
         $self->_clear_fh;
         $self->_clear_csv;
     }
-#    use Data::Printer; p $row;
-    my ( $code, $str, $pos ) = $self->_csv->error_diag;
-    if ( $str && !$self->_csv->eof ) {
 
-        $self->_log_croakf('CSV parse error at pos %s: %s [%s]', $pos, $str, $self->_csv->error_input );
+    #    use Data::Printer; p $row;
+    my ($code, $str, $pos) = $self->_csv->error_diag;
+    if ($str && !$self->_csv->eof) {
+
+        $self->_log_croakf('CSV parse error at pos %s: %s [%s]',
+            $pos, $str, $self->_csv->error_input);
     }
 
     return $row;
 }
 
-method _fetch_data {
+method _fetch_data () {
+
     # does zip already exist?
     # TODO any checksums we can use?
 
-    my $ff = File::Fetch->new( uri => $self->_src_uri );
+    my $ff = File::Fetch->new(uri => $self->_src_uri);
 
-    $self->logger->debug( "Downloading " . $self->_src_uri . " to " . $self->base_dir );
-    my $file = $ff->fetch( to => $self->base_dir )
-      or $self->logger->log_and_croak( $ff->error );
+    $self->logger->debug(
+        "Downloading " . $self->_src_uri . " to " . $self->base_dir);
+    my $file = $ff->fetch(to => $self->base_dir)
+      or $self->logger->log_and_croak($ff->error);
 
-    $self->logger->debug( "Saved data to $file" );
+    $self->logger->debug("Saved data to $file");
 
     return 1;
 }
 
-method _extract_data {
+method _extract_data () {
     if (!$self->_src_file->exists) {
         $self->_fetch_data;
     }
@@ -250,12 +270,12 @@ method _extract_data {
     $self->log->debug('Extracting file ' . $self->_src_file);
     my $zip = Archive::Zip->new;
 
-    unless ( $zip->read( $self->_src_file->stringify ) == AZ_OK ) {
-        $self->logger->error( 'Read error' );
+    unless ($zip->read($self->_src_file->stringify) == AZ_OK) {
+        $self->logger->error('Read error');
         croak;
     }
 
-    my $ok = $zip->extractTree( undef, $self->_data_dir->stringify . '/');
+    my $ok = $zip->extractTree(undef, $self->_data_dir->stringify . '/');
 
     return 1 if $ok == AZ_OK;
     croak "Failed to extract file";
